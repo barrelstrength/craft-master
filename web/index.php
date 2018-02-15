f638b30d7925a0935fb68314700f7d677471c1ba
Ben Parizek <ben@barrelstrengthdesign.com>
2018-02-15 01:55:40 -0500

    Updates index.php
    - Set CRAFT_ENVIRONMENT
    - Set URI_SCHEME
    - Set SITE_URL
    - Remove references to dotenv

--- web/index.php
+++ web/index.php
@@ -3,6 +3,36 @@
  * Craft web bootstrap file
  */
 
+// Set Environment
+switch ($_SERVER['HTTP_HOST']) {    
+  case 'website.com' :
+    define('CRAFT_ENVIRONMENT', 'production');
+    break;
+  case 'dev.website.com' :
+    define('CRAFT_ENVIRONMENT', 'dev');
+    break;
+  default :
+    define('CRAFT_ENVIRONMENT', 'local');
+    break;
+}
+
+// Set Scheme
+if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
+    ## Support proxy SSL
+    define('URI_SCHEME', $_SERVER['HTTP_X_FORWARDED_PROTO'] . '://');
+}
+else if (isset($_SERVER['HTTPS'])) {
+    ## Standard SSL support
+    define('URI_SCHEME', 'https://');
+}
+else {
+    ## Standard without SSL
+    define('URI_SCHEME', 'http://');
+}
+
+// Set Site URL
+define('SITE_URL', URI_SCHEME . $_SERVER['HTTP_HOST'] . '/');
+
 // Set path constants
 define('CRAFT_BASE_PATH', dirname(__DIR__));
 define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH.'/vendor');
@@ -10,12 +40,6 @@ define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH.'/vendor');
 // Load Composer's autoloader
 require_once CRAFT_VENDOR_PATH.'/autoload.php';
 
-// Load dotenv?
-if (file_exists(CRAFT_BASE_PATH.'/.env')) {
-    (new Dotenv\Dotenv(CRAFT_BASE_PATH))->load();
-}
-
 // Load and run Craft
-define('CRAFT_ENVIRONMENT', getenv('ENVIRONMENT') ?: 'production');
 $app = require CRAFT_VENDOR_PATH.'/craftcms/cms/bootstrap/web.php';
 $app->run();
