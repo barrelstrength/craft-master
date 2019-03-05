65647fd0297a4dcb07d75c0504bc61b8f2aced64
Ben Parizek <ben@barrelstrengthdesign.com>
2019-03-04 18:35:13 -0800

    Updates db.php to support .env and multienv

--- config/db.php
+++ config/db.php
@@ -1,43 +1,40 @@
 <?php
 /**
  * Database Configuration
- * Configure all environments
  *
- * See configuration options:
- * vendor/craftcms/cms/src/config/DbConfig.php
+ * All of your system's database connection settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/DbConfig.php.
+ *
+ * @see craft\config\DbConfig
  */
 
-$customDbConfig = [
+return [
 
     // All Environments
     '*' => [
-        'driver' => 'mysql',
-        'port' => '3306',
-        'schema' => 'public',
-        'tablePrefix' => '',
+        'driver' => getenv('DB_DRIVER') ?: 'mysql',
+        'server' => getenv('DB_SERVER') ?: 'localhost',
+        'port' => getenv('DB_PORT') ?: '3306',
+        'user' => getenv('DB_USER') ?: '',
+        'password' => getenv('DB_PASSWORD') ?: '',
+        'database' => getenv('DB_DATABASE') ?: '',
+        'schema' => getenv('DB_SCHEMA') ?: 'public',
+        'tablePrefix' => getenv('DB_TABLE_PREFIX') ?: '',
     ],
 
     // Production Environment
-    'production' => [
-        'server' => 'localhost',
-        'user' => '',
-        'password' => '',
-        'database' => ''
-    ],
+    //'production' => [
+    //    'server'   => 'localhost',
+    //    'user'     => '',
+    //    'password' => '',
+    //    'database' => ''
+    //],
 
     // Dev Environment
-    'dev' => [
-        'server' => 'localhost',
-        'user' => '',
-        'password' => '',
-        'database' => '',
-    ]
+    //'dev' => [
+    //    'server'   => 'localhost',
+    //    'user'     => '',
+    //    'password' => '',
+    //    'database' => '',
+    //]
 ];
-
-## If a local db file exists, merge the local db settings
-if (is_array($customLocalDbConfig = @include(CRAFT_BASE_PATH.'/config/local/db.php'))) {
-    $customGlobalDbConfig = array_merge($customDbConfig['*'], $customLocalDbConfig);
-    $customDbConfig['*'] = $customGlobalDbConfig;
-}
-
-return $customDbConfig;
